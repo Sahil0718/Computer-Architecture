84c2607634d41177def5333e78239783f5f6a8a1
Sahil0718 <135214197+Sahil0718@users.noreply.github.com>
2023-06-05 14:52:42 +0545

    Binary TO Decimal 2080-02-22.cpp

--- Binary TO Decimal 2080-02-22.cpp
+++ Binary TO Decimal 2080-02-22.cpp
@@ -3,23 +3,26 @@
 #include <math.h>
 int convert(long long);
 
-int main() {
-  long long binaryNumber;
-  printf("Enter a binary number: ");
-  scanf("%lld", &binaryNumber);
-  printf("%lld in binary = %d in decimal", binaryNumber, convert(binaryNumber));
-  return 0;
+int main()
+{
+    long long binaryNumber;
+    printf("Enter a binary number: ");
+    scanf("%lld", &binaryNumber);
+    printf("%lld in binary = %d in decimal", binaryNumber, convert(binaryNumber));
+    return 0;
 }
 
-int convert(long long binaryNumber) {
-  int dec = 0, i = 0, rem;
+int convert(long long binaryNumber)
+{
+    int dec = 0, i = 0, rem;
 
-  while (binaryNumber!=0) {
-    rem = binaryNumber % 10;
-    binaryNumber /= 10;
-    dec += rem * pow(2, i);
-    ++i;
-  }
+    while (binaryNumber != 0)
+    {
+        rem = binaryNumber % 10;
+        binaryNumber /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
 
-  return dec;
+    return dec;
 }
